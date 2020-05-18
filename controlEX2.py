c2c9fb62dd7aeeb010a044563fee5932a2dd3b62
hairuizhu <hairuizhu97@gmail.com>
2020-05-18 23:06:17 +0900

    second

--- controlEX2.py
+++ controlEX2.py
@@ -1,17 +1,16 @@
 import os
 
 excutable = "cmake-build-debug/EX2"
-n_thread = ["1", "2", "3", "4", "5", "6", "7", "8"]
+n_thread = ["1", "2", "3", "4"]
 imgInB = "img"
 imgOutB = "imgO"
 
-for imgNum in range(1,5):
+for imgNum in range(1,10):
 	imgIn = imgInB + str(imgNum) + ".bmp"
 	imgOut = imgOutB + str(imgNum) + ".bmp"
 	for num in n_thread:
 		command = excutable + " " + num + " " + imgIn + " " +imgOut
 		for i in range(10):
-			print(command)
 			result = os.system(command)
 
 
